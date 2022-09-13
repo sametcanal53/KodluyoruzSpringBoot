23a3eb6414a138f45f58b22a7085af991e2cf467
Sametcan <sametcanal53@hotmail.com>
2022-09-13 14:51:26 +0300

    PasswordEncoderBean (inactive)

---
+++ src/main/java/com/sametcanal/bean/PasswordEncoderBean.java
@@ -0,0 +1,10 @@
+package com.sametcanal.bean;
+
+//@Configuration
+public class PasswordEncoderBean {
+
+//    @Bean
+//    public PasswordEncoder passwordEncoderBean(){
+//        return new BCryptPasswordEncoder();
+//    }
+}
