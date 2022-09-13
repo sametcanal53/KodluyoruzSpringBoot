ae49fefeceae702c9e2672eb8016464a22284d52
Sametcan <sametcanal53@hotmail.com>
2022-09-13 15:01:14 +0300

    BaseEntity(MappedSuperClass,EntityListeners),EmployeeEntity(Entity,Table), EmployeeDto Revize

---
+++ src/main/java/com/sametcanal/entity/EmployeeEntity.java
@@ -0,0 +1,29 @@
+package com.sametcanal.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+
+@Entity
+@Table(name="employees")
+public class EmployeeEntity extends BaseEntity implements Serializable {
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="email")
+    private String email;
+}
