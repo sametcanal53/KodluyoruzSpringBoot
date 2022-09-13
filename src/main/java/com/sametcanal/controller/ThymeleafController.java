eaf8599b8df3afe9876d3115f377a7cd95b78f4b
Sametcan <sametcanal53@hotmail.com>
2022-09-13 13:36:30 +0300

    Model object, model list, thymeleaf if else

--- src/main/java/com/sametcanal/controller/ThymeleafController.java
+++ src/main/java/com/sametcanal/controller/ThymeleafController.java
@@ -1,42 +1,80 @@
 package com.sametcanal.controller;
 
+import com.sametcanal.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ThymeleafController {
+
 
     // http://localhost:8080/thymeleaf1
     @GetMapping("/thymeleaf1")
     //@ResponseBody
-    public String getThymeleaf1(){
+    public String getThymeleaf1() {
         return "thymeleaf1";
     }
 
+    // Birden fazla model gönderme
     // http://localhost:8080/thymeleaf2
     @GetMapping("/thymeleaf2")
-    public String getThymeleaf2Model(Model model){
-        model.addAttribute("key_model1","Ben modelden geldim-1");
-        model.addAttribute("key_model2","Ben modelden geldim-2");
+    public String getThymeleaf2Model(Model model) {
+        model.addAttribute("key_model1", "Ben modelden geldim-1");
+        model.addAttribute("key_model2", "Ben modelden geldim-2");
         return "thymeleaf1";
     }
 
 
     // http://localhost:8080/thymeleaf3
     @GetMapping("/thymeleaf3")
-    public String getThymeleaf3Model(Model model){
-        model.addAttribute("key_model1","Ben modelden geldim-1");
-        model.addAttribute("key_model2","Ben modelden geldim-2");
+    public String getThymeleaf3Model(Model model) {
+        model.addAttribute("key_model1", "Ben modelden geldim-1");
+        model.addAttribute("key_model2", "Ben modelden geldim-2");
         return "thymeleaf_file/thymeleaf3";
     }
 
     // http://localhost:8080/thymeleaf4
     @GetMapping("/thymeleaf4")
-    public String getThymeleaf4Model(Model model){
-        model.addAttribute("key_model1","Ben modelden geldim-1");
+    public String getThymeleaf4Model(Model model) {
+        model.addAttribute("key_model1", "Ben modelden geldim-1");
         return "thymeleaf4";
+    }
+
+    // Model Object göndermek
+    // http://localhost:8080/thymeleaf5
+    @GetMapping("/thymeleaf5")
+    public String getThymeleaf5Model(Model model) {
+        model.addAttribute("key_model1", "text");
+
+        ProductDto productDto = ProductDto
+                                    .builder()
+                                    .productId(0L)
+                                    .productName("Ürün Adı")
+                                    .productPrice(2500)
+                                    .build();
+
+        model.addAttribute("key_model2",productDto);
+        return "thymeleaf5";
+    }
+
+    // Model Object göndermek
+    // http://localhost:8080/thymeleaf6
+    @GetMapping("/thymeleaf6")
+    public String getThymeleaf6Model(Model model) {
+        model.addAttribute("key_model1", "text");
+        List<ProductDto> list = new ArrayList<>();
+        list.add(ProductDto.builder().productId(1L).productName("Ürün Adı 1").productPrice(1500).build());
+        list.add(ProductDto.builder().productId(2L).productName("Ürün Adı 2").productPrice(2500).build());
+        list.add(ProductDto.builder().productId(3L).productName("Ürün Adı 3").productPrice(3500).build());
+        list.add(ProductDto.builder().productId(4L).productName("Ürün Adı 4").productPrice(4500).build());
+
+        model.addAttribute("product_list",list);
+        return "thymeleaf6";
     }
 
 }
---
+++ src/main/java/com/sametcanal/dto/BaseDto.java
@@ -0,0 +1,8 @@
+package com.sametcanal.dto;
+
+import java.util.Date;
+
+public class BaseDto {
+    private Long id;
+    private Date date;
+}
